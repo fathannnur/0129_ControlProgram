9719037f649b8faefe97308743833b5e869d5a21
fathannnur <carelius02@gmail.com>
2026-03-10 18:58:49 +0700

    first commit

---
+++ file2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a, float b){
+    return (a+b)/2;
+}
+
+string Status_lulus(float r){
+    if (r >= 60)
+        return "Lolos";
+    else if (r >= 50)
+        return "Lolos dipertimbangkan";
+    else if (r >= 40)
+        return "Lolos tetapi matrikulasi 1 tahun";
+    else
+        return "Gagal";
+}
+
+int main(){
+    float nilBI, nilMT, rerata;
+    string status;
+
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    rerata = Rata_rata(nilBI, nilMT);
+    status = Status_lulus(rerata);
+
+    cout << "Status kelulusan = " << status 
+         << ", dengan nilai rerata = " << rerata << endl;
+
+    return 0;
+}
